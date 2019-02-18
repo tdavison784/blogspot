4f04a000835dc8b251f1cf914c825ea34eb373e9
tdavison784 <32623295+tdavison784@users.noreply.github.com>
2019-02-18 07:03:08 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello World!"
+    return "This is a test application running on openshift!"
  
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
