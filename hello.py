2247da41f0729259ba82fa123c7d797f03cf0f61
tdavison784 <32623295+tdavison784@users.noreply.github.com>
2019-02-18 10:56:46 -0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -7,4 +7,4 @@ def hello():
     return "This is a test application running on openshift!"
  
 if __name__ == "__main__":
-    app.run(host='0.0.0.0' port=8080)
+    app.run(host='0.0.0.0', port=8080)
